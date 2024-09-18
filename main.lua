6afbaaba4f4af057cf43187a034ace90c6bc4841
Sieep-Coding <nstamb6@gmail.com>
2024-09-17 20:56:07 -0400

    newUniverse function

--- main.lua
+++ main.lua
@@ -1,8 +1,23 @@
-local N = 25
-local M = 25
-local bools = {"","X"}
+local N = 50
+local M = 50
+local bools = {" ","X"}
 
-local uni = {x = bools[math.random(1,2)], y = bools[math.random(1,2)]}
+local uni = {}
+
+local function newUniverse(density) 
+    local newUni = {}
+    for x=1, N do
+        newUni[x] = {}
+            for y=1, M do
+            if math.random() < density then
+                newUni[x][y] = "X"
+            else
+                newUni[x][y] = " "
+            end
+        end
+    end
+    return newUni
+end
 for x=1, N do
     uni[x] = {}
     for y=1, M do
