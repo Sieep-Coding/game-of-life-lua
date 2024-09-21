99a12b76e42a4e7406e11c2c5d6afb8c19adecf7
Sieep-Coding <nstamb6@gmail.com>
2024-09-20 21:09:46 -0400

    fixed

--- main.lua
+++ main.lua
@@ -10,57 +10,50 @@ local dir = {
     {0, 1}, {-1, 1}, {-1, 0},
 }
 
---Get user input for density
-local function getUserInputDensity(density)
-  
-  local density 
- while true do
-    io.write("Universe Density (Float) ")
-    density = io.read()
-    density = tonumber(density)
-    if density == nil then
-        print("Please enter a number between 0 and 1.")
-    elseif density < 0 or density > 1 then
-        print("Please Enter a number between 0 and 1 ")
-    else
-        break
+-- Get user input for density
+local function getUserInputDensity()
+    local density 
+    while true do
+        io.write("Universe Density (Float between 0 and 1): ")
+        density = io.read()
+        density = tonumber(density)
+        if density == nil then
+            print("Please enter a valid number between 0 and 1.")
+        elseif density < 0 or density > 1 then
+            print("Please enter a number between 0 and 1.")
+        else
+            break
+        end
     end
-end
-    io.write("Selected:", density, "!\n")
+    io.write("Selected density: ", density, "!\n")
     return density
 end
 
-local function getUserInputGeneration(generations)
-  
+-- Get user input for the number of generations
+local function getUserInputGeneration()
     local generations 
-   while true do
-      io.write("Number of Generations (integer) ")
-      generations = io.read()
-      generations = tonumber(generations)
-      if generations == nil then
-          print("Please enter a number.")
-      elseif generations < 0 then
-          print("Please Enter a number greater than 0 ")
-      else
-          break
-      end
-  end
-      io.write("Selected:", generations, "!\n")
-      return generations
-  end
+    while true do
+        io.write("Number of Generations (positive integer): ")
+        generations = io.read()
+        generations = tonumber(generations)
+        if generations == nil then
+            print("Please enter a valid number.")
+        elseif generations <= 0 then
+            print("Please enter a number greater than 0.")
+        else
+            break
+        end
+    end
+    io.write("Selected generations: ", generations, "!\n")
+    return generations
+end
 
---Get Terminal size
--- local function(height)
---   local width = io.popen("")
---   return width,height
--- end
-
---Creates a new universe.
-local function newUniverse(density) 
+-- Creates a new universe with a given density
+local function newUniverse(density)
     local newUni = {}
-    for x=1, N do
+    for x = 1, N do
         newUni[x] = {}
-            for y=1, M do
+        for y = 1, M do
             if math.random() < density then
                 newUni[x][y] = "X"
             else
@@ -71,10 +64,10 @@ local function newUniverse(density)
     return newUni
 end
 
---Uses dir to check for nearby neighbors
+-- Counts the neighbors of a cell at position (x, y)
 local function countNeighbors(universe, x, y)
     local count = 0
-    for i =1, 8 do
+    for i = 1, #dir do
         local row = (x + dir[i][1] - 1) % N + 1
         local col = (y + dir[i][2] - 1) % M + 1
         if universe[row][col] == "X" then
@@ -84,24 +77,25 @@ local function countNeighbors(universe, x, y)
     return count
 end
 
--- update Universe based on the rules of the Game of Life
-local function updateUniverse(universe, density)
-    local newUni = newUniverse(density)
-    for x=1, N do
-        for y=1, M do
+-- Updates the universe based on the rules of the Game of Life
+local function updateUniverse(universe)
+    local newUni = {}
+    for x = 1, N do
+        newUni[x] = {}
+        for y = 1, M do
             local neighbors = countNeighbors(universe, x, y)
             local cell = universe[x][y]
             if cell == "X" then
-                if neighbors < 2 then
+                if neighbors < 2 or neighbors > 3 then
                     newUni[x][y] = " " -- cell dies
-                elseif neighbors > 3 then
-                    newUni[x][y] = " "
-                else 
-                    newUni[x][y] = "X"
+                else
+                    newUni[x][y] = "X" -- cell lives
                 end
             else
                 if neighbors == 3 then
-                    newUni[x][y] = "X"
+                    newUni[x][y] = "X" -- new cell is born
+                else
+                    newUni[x][y] = " "
                 end
             end
         end
@@ -109,27 +103,23 @@ local function updateUniverse(universe, density)
     return newUni
 end
 
+-- Prints the universe in the terminal
 local function printUniverse(universe)
-    for x=1, N do
-        for y=1, M do
-            io.write(tostring(universe[x][y]) .. " ")
+    for x = 1, N do
+        for y = 1, M do
+            io.write(universe[x][y] .. " ")
         end
         print()
     end
 end
 
--- local function sleep(n)
---     if n > 0 then os.execute("ping -n " .. tonumber(n+1) .. " localhost > NUL") end
--- end
-  
-
--- main loop
+-- Main loop
 local density = getUserInputDensity()
 uni = newUniverse(density)
 local generations = getUserInputGeneration()
+
 for gen = 1, generations do
     print("Generation:", gen)
     printUniverse(uni)
-    uni = updateUniverse(uni, density)
-    -- sleep(1)
+    uni = updateUniverse(uni)
 end
